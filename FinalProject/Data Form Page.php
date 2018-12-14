f78550bc53814c061277ec5c8b1dc8f809526a10
Arohi <arohibangera@Arohis-MacBook-Air.local>
2018-12-14 16:59:48 +0400

    Changes

--- FinalProject/Data Form Page.php
+++
@@ -1,9 +0,0 @@
-<html>
-<body>
-
-We have received your query, <?php echo $_POST["name"]; ?>.<br>
-We will get back to you shortly on your provided email address: <?php echo $_POST["email"]; ?><br>
-Thank you and have a nice day!
-
-</body>
-</html>
